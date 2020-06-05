db571e4c31500d3d9cb6c720b5920a39cb5bd217
Nikita Miloserdov <nmiloserdov09@gmail.com>
2020-06-05 19:40:29 +0100

    Move styles to another directory

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,3 @@
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-draw/dist/leaflet.draw.css';
-import 'toastify-js/src/toastify.css';
 
 const componentRequireContext = require.context('components', true);
 const ReactRailsUJS = require('react_ujs');
